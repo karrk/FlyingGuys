9dbf647ccc89bafaadb70e45a78c2551b21d694d
karrk <101902544+karrk@users.noreply.github.com>
2024-11-23 04:40:17 +0900

    [Feat] 사운드 매니저 기능 구현

---
+++ Assets/1.Private/JangDJ/Scripts/Sounds/AudioList.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AudioList<T> : MonoBehaviour where T : Enum
+{
+    [SerializeField] protected List<ClipContent<T>> _list;
+    public AudioSource Source => GetComponent<AudioSource>();
+
+    public AudioClip this[T type]
+    {
+        get { return GetClip(type); }
+    }
+
+    private AudioClip GetClip(T type)
+    {
+        for (int i = 0; i < _list.Count; i++)
+        {
+            if (type.ToString() == _list[i].Name.ToString())
+                return _list[i].Clip;
+        }
+
+        Debug.LogError($"{type} 음원을 찾을 수 없습니다.");
+        return null;
+    }
+}
+
+[System.Serializable]
+public class ClipContent<T> where T : Enum
+{
+    public T Name;
+    public AudioClip Clip;
+}
---
+++ Assets/1.Private/JangDJ/Scripts/Sounds/SFXList.cs
@@ -0,0 +1,3 @@
+public class SFXList : AudioList<E_SFX>
+{
+}
