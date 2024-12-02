4a924f3e272a18bfd29c1181e141e41f2e2a95ab
jiyeonUning <yeon14735@gmail.com>
2024-12-02 19:43:22 +0900

    fix / UI SFX 효과음 작업 완료

--- Assets/1.Private/JangDJ/Scripts/Manager/UIManager.cs
+++ Assets/1.Private/JangDJ/Scripts/Manager/UIManager.cs
@@ -1,13 +1,54 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour, IManager
 {
     public static UIManager Instance { get; private set; }
+    [SerializeField] Button[] Buttons;
+
+    private Coroutine cor;
 
     public void Init()
     {
         Instance = this;
+    }
+
+
+    void Awake()
+    {
+        SceneManager.sceneLoaded += ButtonSFX;
+    }
+
+    void LateUpdate()
+    {
+        StartCoroutine(BGMPlay());
+    }
+
+    void ButtonSFX(Scene s, LoadSceneMode m)
+    {
+        Array.Clear(Buttons, 0, Buttons.Length);
+        Buttons = FindObjectsOfType<Button>();
+
+        foreach (Button button in Buttons)
+        {
+            button.AddComponent<Audio_SFXSpeaker>();
+        }
+    }
+
+    // ㅠㅠ
+    IEnumerator BGMPlay()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Public_Login": SoundManager.Instance.Play(E_BGM.BG_Login, 0f, 0f); yield break;
+            case "Public_Menu": SoundManager.Instance.Play(E_BGM.BG_MainMenu, 0f, 0f); yield break;
+            case "Public_Loading": SoundManager.Instance.Play(E_BGM.BG_Loading, 0f, 0f); yield break;
+            case "Public_Result": SoundManager.Instance.Play(E_BGM.BG_GameResult, 0f, 0f); yield break;
+            default: yield break;
+        }
     }
 }
