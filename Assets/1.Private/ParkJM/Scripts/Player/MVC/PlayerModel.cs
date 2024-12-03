6cca899c830ab829d90d2ee1d515bf235a49a303
LemoZun <marrzun@gmail.com>
2024-12-03 16:58:07 +0900

    chore : 주석 첨삭

--- Assets/1.Private/ParkJM/Scripts/Player/MVC/PlayerModel.cs
+++ Assets/1.Private/ParkJM/Scripts/Player/MVC/PlayerModel.cs
@@ -8,14 +8,11 @@ public class PlayerModel
 {
     public int playerNumber;
     public float moveSpeed;
-    public float maxSpeed;
-    public float maxAccel;
     public float moveSpeedInAir;
     public float jumpForce;
     public float divingForce;
     public float grabRadius;
     public float grabForce;
-
 
     public event Action OnPlayerJumped;
     public event Action OnPlayerDove;
