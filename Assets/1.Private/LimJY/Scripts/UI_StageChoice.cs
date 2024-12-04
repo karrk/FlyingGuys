1acb94c746ab0e2f65171870251552999b1018e1
jiyeonUning <yeon14735@gmail.com>
2024-12-04 11:12:09 +0900

    fix / LoadingScene 각 스테이지 이미지&설명 출력 기능 보완

--- Assets/1.Private/LimJY/Scripts/UI_StageChoice.cs
+++ Assets/1.Private/LimJY/Scripts/UI_StageChoice.cs
@@ -56,7 +56,7 @@ public class UI_StageChoice : MonoBehaviourPun
     [PunRPC]
     void Mountine()
     {
-        CurScreenShot.sprite = MapScreenShot[playMath.num];
+        CurScreenShot.sprite = MapScreenShot[0];
         CossName.text = "산 무너져유";
         CossEx.text = "왕관에 가장 먼저 닿는 플레이어가 승리합니다!";
     }
@@ -64,7 +64,7 @@ public class UI_StageChoice : MonoBehaviourPun
     [PunRPC]
     void Floor()
     {
-        CurScreenShot.sprite = MapScreenShot[playMath.num];
+        CurScreenShot.sprite = MapScreenShot[1];
         CossName.text = "바닥 떨어져유";
         CossEx.text = "타일을 밟고 난 후 일정 시간이 경과할 때 마다, 바닥 타일이 하나씩 사라집니다. \n\n떨어지지 않고 가장 오래 살아남는 플레이어가 승리합니다!";
     }
@@ -72,7 +72,7 @@ public class UI_StageChoice : MonoBehaviourPun
     [PunRPC]
     void Jump()
     {
-        CurScreenShot.sprite = MapScreenShot[playMath.num];
+        CurScreenShot.sprite = MapScreenShot[2];
         CossName.text = "점프 쇼다운";
         CossEx.text = "돌아가는 장애물을 피해서 가장 오래 살아남는 플레이어가 승리합니다! \n\n장애물은 시간이 지날수록 속도가 빨라집니다.";
     }
@@ -80,7 +80,7 @@ public class UI_StageChoice : MonoBehaviourPun
     [PunRPC]
     void Roll()
     {
-        CurScreenShot.sprite = MapScreenShot[playMath.num];
+        CurScreenShot.sprite = MapScreenShot[3];
         CossName.text = "롤 아웃";
         CossEx.text = "굴러가는 바닥 위에서 떨어지지 않고, 가장 오래 살아남는 플레이어가 승리합니다!";
     }
